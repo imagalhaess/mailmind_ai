f5deeeac4cb5d7d0bd8aa94794d8663a5039a1c4
imagalhaess <magalhaes569@gmail.com>
2025-10-05 22:31:08 -0300

    fix: corrige indentação que causava erro no deploy.

--- wsgi.py
+++ wsgi.py
@@ -8,3 +8,5 @@ app = create_app()
 application = app
 
 if __name__ == "__main__":
+    # Executa a aplicação quando o arquivo é chamado diretamente
+    app.run(debug=False, host='0.0.0.0', port=8080)
